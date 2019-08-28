b89b4226b4911ea05f9106be4b5cc96929abc365
Joaquín <joca_323@hotmail.com>
2019-08-28 18:36:45 -0300

    Cambio 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApp1
             for (int i = 0; i <= 10; i++)
             {
                 Console.WriteLine(i);
+                //Some changes
             }
             Console.ReadLine();//Pausa chapusera
         }
